5e3d4fcd2357e179e4a7aa829f92b43bd7c71240
rajusustogi <C@nstrue1>
2021-10-12 23:11:17 +0530

    Book now button added

--- App/hotelapp/lib/main.dart
+++ App/hotelapp/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hotelapp/screens/HomeScreen.dart';
 import 'package:hotelapp/screens/login_screen.dart';
 
 void main() {
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: LoginPage(),
     );
   }
 }
